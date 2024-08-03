1452c43ab783316dfc57b2357367698d6ad6bde1
Stephen Sun <stasun@ucsc.edu>
2024-08-02 21:41:38 -0700

    finished madhava.c

---
+++ School Projects/cse13s/asgn2/madhava.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "mathlib.h"
+
+static int terms = -1;
+
+double pi_madhava(void) {
+	terms = 0;
+	double summation = 0.0;
+	double next_term = 0.0;
+	double pi = 0.0;
+	while (absolute(M_PI - pi) >= EPSILON) {
+		next_term = 1.0 / (2.0 * terms + 1.0);
+		for (int i = 0; i < terms; i++) {	
+			next_term /= -3.0;
+		}
+
+		summation += next_term;
+		pi = sqrt_newton(12) * summation;
+		terms++;
+	}
+
+	return pi;
+}
+
+int pi_madhava_terms(void) {
+	if (terms < 0) {
+		fprintf(stderr, "ERROR: pi_madhava() had not been previously called\n");
+	}	
+
+	return terms;
+}
+
+int main(void) {
+	printf("pi: %.15f, iters: %d\n", pi_madhava(), pi_madhava_terms());
+	
+	return 0;
+}
